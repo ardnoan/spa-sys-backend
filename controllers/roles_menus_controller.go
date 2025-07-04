ae00373a137c4c32359e02616360071941d5fa2f
Ardnoan <ardin218120065@gmail.com>
2025-07-04 13:40:01 +0700

    Update

--- controllers/roles_menus_controller.go
+++ controllers/roles_menus_controller.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq"
@@ -14,19 +15,19 @@ type RoleMenusController struct {
 }
 
 type RoleMenu struct {
-	RoleMenuID  int    `json:"role_menu_id"`
-	RoleID      int    `json:"role_id"`
-	RoleName    string `json:"role_name"`
-	MenuID      int    `json:"menu_id"`
-	MenuName    string `json:"menu_name"`
-	CanView     bool   `json:"can_view"`
-	CanCreate   bool   `json:"can_create"`
-	CanModify   bool   `json:"can_modify"`
-	CanDelete   bool   `json:"can_delete"`
-	CanUpload   bool   `json:"can_upload"`
-	CanDownload bool   `json:"can_download"`
-	CreatedAt   string `json:"created_at"`
-	CreatedBy   *int   `json:"created_by"`
+	RoleMenuID  int       `json:"role_menu_id"`
+	RoleID      int       `json:"role_id"`
+	RoleName    string    `json:"role_name"`
+	MenuID      int       `json:"menu_id"`
+	MenuName    string    `json:"menu_name"`
+	CanView     bool      `json:"can_view"`
+	CanCreate   bool      `json:"can_create"`
+	CanModify   bool      `json:"can_modify"`
+	CanDelete   bool      `json:"can_delete"`
+	CanUpload   bool      `json:"can_upload"`
+	CanDownload bool      `json:"can_download"`
+	CreatedAt   time.Time `json:"created_at"`
+	CreatedBy   *string   `json:"created_by"` // ✅ sudah fix
 }
 
 func NewRoleMenusController(db *sql.DB) *RoleMenusController {
@@ -47,11 +48,11 @@ func (c *RoleMenusController) GetAllRoleMenus(ctx echo.Context) error {
 
 	offset := (page - 1) * limit
 
-	var countQuery string
-	var query string
+	var countQuery, dataQuery string
 	var args []interface{}
 
 	if search != "" {
+		searchParam := "%" + search + "%"
 		countQuery = `
 			SELECT COUNT(*) 
 			FROM role_menus rm
@@ -59,11 +60,10 @@ func (c *RoleMenusController) GetAllRoleMenus(ctx echo.Context) error {
 			JOIN menus m ON rm.menu_id = m.menus_id
 			WHERE ur.roles_name ILIKE $1 OR m.menu_name ILIKE $1
 		`
-
-		query = `
+		dataQuery = `
 			SELECT rm.role_menu_id, rm.role_id, ur.roles_name, rm.menu_id, m.menu_name,
-				   rm.can_view, rm.can_create, rm.can_modify, rm.can_delete, 
-				   rm.can_upload, rm.can_download, rm.created_at, rm.created_by
+			       rm.can_view, rm.can_create, rm.can_modify, rm.can_delete, 
+			       rm.can_upload, rm.can_download, rm.created_at, rm.created_by
 			FROM role_menus rm
 			JOIN users_roles ur ON rm.role_id = ur.roles_id
 			JOIN menus m ON rm.menu_id = m.menus_id
@@ -71,7 +71,6 @@ func (c *RoleMenusController) GetAllRoleMenus(ctx echo.Context) error {
 			ORDER BY ur.roles_name, m.menu_name
 			LIMIT $2 OFFSET $3
 		`
-		searchParam := "%" + search + "%"
 		args = []interface{}{searchParam, limit, offset}
 	} else {
 		countQuery = `
@@ -80,11 +79,10 @@ func (c *RoleMenusController) GetAllRoleMenus(ctx echo.Context) error {
 			JOIN users_roles ur ON rm.role_id = ur.roles_id
 			JOIN menus m ON rm.menu_id = m.menus_id
 		`
-
-		query = `
+		dataQuery = `
 			SELECT rm.role_menu_id, rm.role_id, ur.roles_name, rm.menu_id, m.menu_name,
-				   rm.can_view, rm.can_create, rm.can_modify, rm.can_delete, 
-				   rm.can_upload, rm.can_download, rm.created_at, rm.created_by
+			       rm.can_view, rm.can_create, rm.can_modify, rm.can_delete, 
+			       rm.can_upload, rm.can_download, rm.created_at, rm.created_by
 			FROM role_menus rm
 			JOIN users_roles ur ON rm.role_id = ur.roles_id
 			JOIN menus m ON rm.menu_id = m.menus_id
@@ -95,21 +93,24 @@ func (c *RoleMenusController) GetAllRoleMenus(ctx echo.Context) error {
 	}
 
 	var totalRecords int
-	if search != "" {
-		err := c.DB.QueryRow(countQuery, "%"+search+"%").Scan(&totalRecords)
-		if err != nil {
-			return ctx.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to count records"})
-		}
-	} else {
-		err := c.DB.QueryRow(countQuery).Scan(&totalRecords)
-		if err != nil {
-			return ctx.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to count records"})
-		}
+	countArgs := args
+	if search == "" {
+		countArgs = []interface{}{}
+	}
+	err := c.DB.QueryRow(countQuery, countArgs...).Scan(&totalRecords)
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"error":  "Failed to count records",
+			"detail": err.Error(),
+		})
 	}
 
-	rows, err := c.DB.Query(query, args...)
+	rows, err := c.DB.Query(dataQuery, args...)
 	if err != nil {
-		return ctx.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to fetch role menus"})
+		return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"error":  "Failed to fetch role menus",
+			"detail": err.Error(),
+		})
 	}
 	defer rows.Close()
 
@@ -132,14 +133,17 @@ func (c *RoleMenusController) GetAllRoleMenus(ctx echo.Context) error {
 			&roleMenu.CreatedBy,
 		)
 		if err != nil {
-			return ctx.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to scan role menu"})
+			return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+				"error":  "Failed to scan role menu",
+				"detail": err.Error(),
+			})
 		}
 		roleMenus = append(roleMenus, roleMenu)
 	}
 
 	totalPages := (totalRecords + limit - 1) / limit
 
-	response := map[string]interface{}{
+	return ctx.JSON(http.StatusOK, map[string]interface{}{
 		"data": roleMenus,
 		"pagination": map[string]interface{}{
 			"current_page":     page,
@@ -147,7 +151,5 @@ func (c *RoleMenusController) GetAllRoleMenus(ctx echo.Context) error {
 			"total_records":    totalRecords,
 			"records_per_page": limit,
 		},
-	}
-
-	return ctx.JSON(http.StatusOK, response)
+	})
 }
